810a181f5642c4c5af8f16414932ba710bfa0297
Anoop Simon <s451421990@gmail.com>
2021-08-24 19:47:04 +1000

    added few more methods

--- core/src/main/java/core/utils/ExcelUtil.java
+++ core/src/main/java/core/utils/ExcelUtil.java
@@ -9,6 +9,12 @@ import java.util.List;
 
 public class ExcelUtil {
 
+    /**
+     * Read an excel workbook , and return the sheet provided in sheetName parameter
+     * @param filePath
+     * @param sheetName
+     * @return sheet as a list of rows
+     */
     public static List<Row> getSheet(String filePath, String sheetName)
     {
         try (InputStream is = new FileInputStream(new File(filePath)); ReadableWorkbook wb = new ReadableWorkbook(is))
@@ -17,7 +23,23 @@ public class ExcelUtil {
             return sheet.read();
 
         } catch (Exception e) {
-            throw new RuntimeException("Failed to read sheet from excel " + e.getMessage());
+            throw new RuntimeException("Failed to read sheet from excel =>" + e.getMessage());
+        }
+    }
+
+    /**
+     * Read an excel file into readable workbook
+     * @param filePath
+     * @return
+     */
+    public static ReadableWorkbook getWorkbook(String filePath)
+    {
+        try (InputStream is = new FileInputStream(new File(filePath)); ReadableWorkbook wb = new ReadableWorkbook(is))
+        {
+            return wb;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read workbook => " + e.getMessage());
         }
     }
 }
--- core/src/main/java/core/utils/WebDriverUtil.java
+++ core/src/main/java/core/utils/WebDriverUtil.java
@@ -2,25 +2,64 @@ package core.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * Hello world!
  */
 public class WebDriverUtil {
 
-    private static WebDriver driver;
-    public static WebDriver initializeSession(String url)
+    private  WebDriver driver;
+
+    public  WebDriver initChromeLocal(String url)
     {
+        if(isBlank(System.getProperty("webdriver.chrome.driver", "")))
+            throw new RuntimeException("Chrome driver must be set in System property as 'webdriver.chrome.driver'");
+
         System.out.println("Launching web browser session...");
 
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions=new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
+
+        driver.manage().window().maximize();
+
         driver.get(url);
         return driver;
     }
 
-    public static void killSession()
+    /**
+     * Initialize Chrome Browser session in Selenium Grid infrastructure
+     * @param gridUrl
+     * @param chromeOptions
+     * @return
+     */
+    public WebDriver initChromeInGrid(String gridUrl, ChromeOptions chromeOptions)
+    {
+        try
+        {
+            driver = new RemoteWebDriver(new URL(gridUrl),chromeOptions);
+            driver.manage().window().maximize();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return driver;
+    }
+
+    /**
+     * Terminate Webdriver session
+     */
+    public void killSession()
     {
         System.out.println("Killing web browser session...");
+        if(driver==null) return;
+
         driver.quit();
     }
 }
