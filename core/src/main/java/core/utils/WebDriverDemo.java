610694a5a2ae10906577fcbbcd89f5126c40f97b
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2021-11-18 14:18:21 +1100

    updated code to capture http requests

--- core/src/main/java/core/utils/WebDriverDemo.java
+++ core/src/main/java/core/utils/WebDriverDemo.java
@@ -3,19 +3,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v95.network.Network;
-import org.openqa.selenium.devtools.v95.network.model.Headers;
 import org.openqa.selenium.devtools.v95.network.model.RequestId;
 import org.openqa.selenium.devtools.v95.network.model.ResourceType;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 public class WebDriverDemo
 {
-
-
     static ChromeDriver chromeDriver;
     static DevTools devTools;
     public static void main(String[] args) throws InterruptedException {
@@ -35,16 +30,7 @@ public class WebDriverDemo
             if(response.getType()== ResourceType.XHR) {
 
                 RequestId requestId = response.getRequestId();
-
-//                System.out.println("************************");
-//                System.out.println("URL : " + response.getResponse().getUrl());
-//                Headers headers = response.getResponse().getHeaders();
-//                System.out.println(headers);
-//                System.out.println(response.getType().toJson());
                String body = devTools.send(Network.getResponseBody(requestId)).getBody();
-//                System.out.println("Body :" + body);
-//                System.out.println("************************");
-
 
                 //add to list
                 WebResponse webresponse=new WebResponse();
