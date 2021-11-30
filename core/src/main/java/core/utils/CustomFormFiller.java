32a127ce11be4037f002d318850446efbdaa5e7a
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2021-11-30 11:32:15 +1100

    added fakefille logic to handle multiple sheets

--- core/src/main/java/core/utils/CustomFormFiller.java
+++ core/src/main/java/core/utils/CustomFormFiller.java
@@ -7,6 +7,12 @@ import org.slf4j.LoggerFactory;
 public class CustomFormFiller extends FakeFiller{
     private static final Logger logger = LoggerFactory.getLogger(CustomFormFiller.class);
     WebDriver driver;
+    String p= this.getClass().getClassLoader().getResource("fakefiller.properties").getPath();
+
+    public static void main(String[] args)
+    {
+        System.out.println(new CustomFormFiller().p);
+    }
 
 //    @Override
 //    public void fillForm(WebDriver driver , String filepath,String testcase)
