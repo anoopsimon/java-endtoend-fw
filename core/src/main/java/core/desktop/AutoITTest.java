2f40c0b1007699ec63c2b98b819a8fcc0470ec75
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2021-12-07 22:10:22 +1100

    added auto it

---
+++ core/src/main/java/core/desktop/AutoITTest.java
@@ -0,0 +1,9 @@
+package core.desktop;
+
+import java.io.IOException;
+
+public class AutoITTest {
+    public static void main(String[] args) throws IOException {
+        new AutoItXInstaller().initAutoIt();
+    }
+}
---
+++ core/src/test/java/core/utils/test/AutoITTest.java
@@ -0,0 +1,11 @@
+package core.utils.test;
+
+import core.desktop.AutoItXInstaller;
+
+import java.io.IOException;
+
+public class AutoITTest {
+    public static void main(String[] args) throws IOException {
+        new AutoItXInstaller().initAutoIt();
+    }
+}
